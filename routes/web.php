00fdf50e2ed357af600489f4b1146f064ecb77da
Davidasrobot <yogasakajr@gmail.com>
2020-05-19 06:48:56 +0700

    add clear cache route

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,29 @@ Route::get('/booking/success', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+
+// CACHE CLEARER
+//Clear route cache:
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return response()->json(['message' => 'Routes cache cleared', $exitCode]);
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return response()->json(['message' => 'Config cache cleared', $exitCode]);
+}); 
+
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return response()->json(['message' => 'Application cache cleared', $exitCode]);
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return response()->json(['message' => 'View cache cleared', $exitCode]);
+});
